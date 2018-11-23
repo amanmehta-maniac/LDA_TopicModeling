c4dba6b9501d384aff7cdfbc1fd95ee8f6c320ba
amanmehta-maniac <amanmehta1997@gmail.com>
2018-11-23 19:19:47 +0530

    first basic code

---
+++ auxilaryFunctions.py
@@ -0,0 +1,12 @@
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
